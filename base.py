564f486e3c92a1615fa93e58c8fc0a87f7eff5f6
saul205 <45789194+saul205@users.noreply.github.com>
2019-03-01 17:39:09 +0100

    post

--- base.py
+++ base.py
@@ -1,134 +1,175 @@
 import os
-import numpy as np
+#import numpy as np
+
+
+
+#input = 'b_lovely_landscapes.txt'
+#input = 'c_memorable_moments.txt'
+input = 'd_pet_pictures.txt'
+#input = 'e_shiny_selfies.txt'
+
+#output = 'b_example.out'
+#output = 'c_example.out'
+output = 'd_example.out'
+#output = 'e_example.out'
+
+
 import random as r
-from heapq import merge
-
-#input = 'a_example.txt'
-input = 'b_lovely_landscapes.txt'
-#output_file = 'a_example.out'
-output_file = 'b_lovely_landscapes.out'
-
-interes = 1
-
-with open(input) as f_in:
-    # read first line
-    numfotos = [int(x) for x in next(f_in).split()]
-    matrix = []
-    for line in f_in: # read rest of lines
-        matrix.append([x for x in line.split()])
-    V = []
-    slides = []
-    integercounter = 0
-    for element in matrix:
-        tmp = []
-        tmptags = []
-        tmp.append(str(integercounter))
-        for i in range(int(element[1])):
-            tmptags.append(element[2+i])
-        tmp.append(tmptags)
-        if element[0] is 'V':
-            V.append(tmp)
-        elif element[0] is 'H':
-            slides.append(tmp)
-        integercounter = integercounter + 1
-    del matrix
-
 
 def factor_interes(slide1, slide2):
-    esta = 0
-    for i in slide1[1]:
-        if i in slide2[1]:
-            esta += 1
-
-    return min([esta, len(slide1[1])-esta, len(slide2[1])-esta])
-
+    encontrados = 0
+    encontrado = False
+    i = 0
+    while i < slide1[1] and not encontrado:
+        if slide1[2][i] in slide2[2]:
+            encontrados += 1
+            if encontrados >= 3:
+                encontrado = True
+        i += 1
+    return encontrado
 
 def enlazar():
-    #global slides
+    global slides
     salida = []
-    salida.append(slides.pop(r.randint(0, len(slides)-1)))
+    long = len(slides)
+    salida.append(slides.pop(0))
 
     while len(slides) > 1:
-        i = 0
-        encontrado = False
-        while i < len(slides) and not encontrado:
-            if factor_interes(salida[-1], slides[i]) >= interes:
-                print("%s\n" % len(slides))
-                salida.append(slides[i])
-                slides.remove(slides[i])
-                encontrado = True
+            encontrado = False
+            i = 0
+            
+            while i < len(slides) and not encontrado:
 
-            i+=1
+                if factor_interes(salida[-1], slides[i]):
+                    salida.append(slides[i])
+                    del slides[i]
+                    encontrado = True
+                    if len(slides)% 500:
+                        print(len(slides))
+
+                i += 1
+
+            if not encontrado:
+                salida.append(slides.pop())
 
     salida.append(slides[0])
+
     return salida
 
-
-
-def matching(photo1, photo2):
-    matchingsi = 0
-    for i in range(len(photo1[1])):
-        for j in range(len(photo2 [1])):
-            if(photo1[1][i] == photo2[1][j]):
-                matchingsi+= 1
-    return matchingsi
-
+def burbuja(lista):
+    num = len(lista)
+    i = 0
+    while i < num:
+          j = i
+          while j < num:
+                  if lista[i] > lista[j]:
+                          aux = lista[i]
+                          lista[i] = lista[j]
+                          lista[j] = aux
+                  j = j + 1
+          i = i + 1
+    
 def emparejar():
-    maximo = 99999999
-    #numrandom= r.randint(0,len(V)-1)
-    numrandom = 0
-    random = V.pop(numrandom)
-    elegida = V.pop()
-    #numeroelegida = 0
-    for i in range(0, len(V)-1):
-        numero = matching(random, V[i])
-        if numero < maximo:
-            maximo = numero
-            V.append(elegida)
-            elegida = V.pop(i)
+    
+    a = V[0][0] + ' ' + V[len(V)-1][0]
+    V[0][2].extend([element for element in V[len(V)-1][2] if element not in V[0][2]])
+    slides.append([a,len(V[0][2]),V[0][2]])
+    del V[len(V)-1]
+    del V[0]
+    
+with open(input) as f_in:
+
+    # read first line
+
+    numfotos = [int(x) for x in next(f_in).split()]
+
+    matrix = []
+
+    for line in f_in: # read rest of lines
+
+        matrix.append([x for x in line.split()])
 
 
-    final = []
-    final.append(random[0] + ' ' + elegida[0])
-    final.append(list(merge(random[1], elegida[1])))
 
-    return final
+    V = []
+
+    slides = []
+
+    integercounter = 0
+
+    for element in matrix:
+
+        tmp = []
+
+        tmptags = []
+
+        tmp.append(str(integercounter))
+        tmp.append(int(element[1]))
+
+        for i in range(int(element[1])):
+
+            tmptags.append(element[2+i])
+
+        tmp.append(tmptags)
+
+        if element[0] is 'V':
+
+            V.append(tmp)
+
+        elif element[0] is 'H':
+
+            slides.append(tmp)
+
+        integercounter = integercounter + 1
 
 
 
-def output(res):
-    with open(output_file, 'w') as f_out:
-        f_out.write("%s\n" % len(res))
-        for item in res:
-            #f_out.write("%s " % item[0])
+
+
+
+
+
+
+def testoutput(salida):
+
+    with open(output, 'w') as f_out:
+        f_out.write(str(len(salida)))
+        f_out.write('\n')
+
+        for item in salida:
+            
             f_out.write(item[0])
             f_out.write("\n")
 
 
 
-def testoutput():
-    with open(output, 'w') as f_out:
-        for item in V:
-            for subitem in item:
-                f_out.write("%s " % subitem)
-            f_out.write("\n")
-        for item in slides:
-            for subitem in item:
-                f_out.write("%s " % subitem)
-            f_out.write("\n")
 
 
 def main():
-    #Codigo aqui
+    print(len(slides))
+    '''if len(V) > 2:
+        
+        burbuja(V)
+    '''
+    print("emparejando")
+    while len(V) > 1:
+        emparejar();
+    salida = []
+    for i in V:
+        salida.append(i)
 
-    while len(V) >= 1:
-        slides.append(emparejar())
+    print("enlazando\n")
+    salida.extend(enlazar())
+    print(len(salida))
+    print(len(slides))
+    print("escribiendo\n")
 
-    output_value = enlazar()
+    testoutput(salida)
 
-    output(output_value)
-    #testoutput()
+
+
 
 
 if __name__ == '__main__':
+
     main()
